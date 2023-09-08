df1c74e18fa8423d7d7e649e27437896a9bf3a89
Patrick <patrickmanzon9@gmail.com>
2023-09-08 12:00:48 +0800

    added database sql

---
+++ database.sql
@@ -0,0 +1,42 @@
+DROP SCHEMA IF EXISTS `crud_project_spring`;
+
+CREATE SCHEMA `crud_project_spring`;
+
+use `crud_project_spring`;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+
+DROP TABLE IF EXISTS `instructor`;
+
+CREATE TABLE `instructor` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `course`;
+
+CREATE TABLE `course` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(128) DEFAULT NULL,
+  `instructor_id` int DEFAULT NULL,
+  
+  PRIMARY KEY (`id`),
+  
+  UNIQUE KEY `TITLE_UNIQUE` (`title`),
+  
+  KEY `FK_INSTRUCTOR_idx` (`instructor_id`),
+  
+  CONSTRAINT `FK_INSTRUCTOR` 
+  FOREIGN KEY (`instructor_id`) 
+  REFERENCES `instructor` (`id`) 
+  
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+
+SET FOREIGN_KEY_CHECKS = 1;
